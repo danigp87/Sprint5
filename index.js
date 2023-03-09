0e3c27d574237ff98018d20958e3da49b2ca889e
Dani Portillo <danigarciaportillo@gmail.com>
2023-03-09 10:35:19 +0100

    Nivel 1 Completado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
-const API_URL = "https://icanhazdadjoke.com/api";
 /* document.getElementById("nextJoke")!.addEventListener("click", nextJoke) //el botón en el html lleva una id que sería nextJoke */
+const API_URL = "https://icanhazdadjoke.com/api";
 function nextJoke() {
     fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -10,5 +10,19 @@ function nextJoke() {
         .then(response => response.json())
         .then(json => document.getElementById("theJoke").innerHTML = json.joke)
         .catch(error => console.log(error));
-    /* const theJoke: HTMLElement | any = document.getElementById('theJoke') */
+}
+const reportJoke = [];
+function score(number) {
+    var _a;
+    const joke = (_a = document.getElementById('theJoke')) === null || _a === void 0 ? void 0 : _a.innerHTML;
+    reportJoke.forEach((item, index) => {
+        if (item.joke === joke) {
+            reportJoke.splice(index, 1);
+        }
+    });
+    const score = number;
+    const date = new Date().toISOString();
+    const scoredJoke = { joke, score, date };
+    reportJoke.push(scoredJoke);
+    console.log(reportJoke);
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-const API_URL: string = "https://icanhazdadjoke.com/api"
 /* document.getElementById("nextJoke")!.addEventListener("click", nextJoke) //el botón en el html lleva una id que sería nextJoke */
 
+const API_URL: string = "https://icanhazdadjoke.com/api"
 
 
 function nextJoke() {
@@ -14,7 +14,29 @@ function nextJoke() {
     .then(json => document.getElementById("theJoke")!.innerHTML = json.joke)
     .catch(error => console.log(error))
 
-
-  /* const theJoke: HTMLElement | any = document.getElementById('theJoke') */
-
 }
+
+interface Joke {
+  joke: string;
+  score: number;
+  date: string;
+}
+
+const reportJoke: Joke[] = []
+
+function score(number: number) {
+  const joke: HTMLElement | any = document.getElementById('theJoke')?.innerHTML
+
+  reportJoke.forEach((item, index) => {
+    if (item.joke === joke) {
+      reportJoke.splice(index, 1)
+    }
+})
+
+  const score = number
+  const date: string = new Date().toISOString()
+  const scoredJoke = { joke, score, date }
+  reportJoke.push(scoredJoke)
+  console.log(reportJoke)
+}
+
